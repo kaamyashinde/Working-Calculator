4ed33991f0bb8363cc5a574b597a00577e58e840
kaamyashinde <kaamyashinde@gmail.com>
2025-03-08 18:40:39 +0100

    remove unecessary pagination on calc log

--- calculator-frontend/src/components/CalcLog.vue
+++ calculator-frontend/src/components/CalcLog.vue
@@ -1,50 +1,17 @@
 <script setup>
-import { ref, watch, computed } from 'vue'
-
-const props = defineProps({
+defineProps({
   resultArray: {
     type: Array,
     required: true,
   },
 })
-
-const currentPage = ref(0)
-const itemsPerPage = 10
-
-const totalPages = ref(0)
-watch(() => props.resultArray, (newArray) => {
-  totalPages.value = Math.ceil(newArray.length / itemsPerPage)
-}, { immediate: true })
-
-const paginatedResults = computed(() => {
-  const start = currentPage.value * itemsPerPage
-  const end = start + itemsPerPage
-  return props.resultArray.slice(start, end)
-})
-
-const nextPage = () => {
-  if (currentPage.value < totalPages.value - 1) {
-    currentPage.value++
-  }
-}
-
-const prevPage = () => {
-  if (currentPage.value > 0) {
-    currentPage.value--
-  }
-}
 </script>
 
 <template>
   <div class="calc-log">
     <ul>
-      <li v-for="(result, index) in paginatedResults" :key="index">{{ result }}</li>
+      <li v-for="(result, index) in resultArray" :key="index">{{ result }}</li>
     </ul>
-    <div class="pagination">
-      <button :disabled="currentPage === 0" @click="prevPage">Previous</button>
-      <span>Page {{ currentPage + 1 }} of {{ totalPages }}</span>
-      <button :disabled="currentPage >= totalPages - 1" @click="nextPage">Next</button>
-    </div>
   </div>
 </template>
 
@@ -73,38 +40,5 @@ li {
 
 li:last-child {
   border-bottom: none;
-}
-
-.pagination {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-top: 1rem;
-  padding-top: 1rem;
-  border-top: 1px solid #e2e8f0;
-}
-
-.pagination button {
-  padding: 0.5rem 1rem;
-  border: none;
-  border-radius: 4px;
-  background-color: #4a90e2;
-  color: white;
-  cursor: pointer;
-  transition: background-color 0.3s;
-}
-
-.pagination button:hover:not(:disabled) {
-  background-color: #357abd;
-}
-
-.pagination button:disabled {
-  background-color: #cbd5e0;
-  cursor: not-allowed;
-}
-
-.pagination span {
-  color: #2c3e50;
-  font-size: 0.9rem;
 }
 </style>
